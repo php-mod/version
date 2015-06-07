f716c9ca86f73c21b45e86182162b592e95cce9d
Hassan Amouhzi <github@anezi.net>
2015-06-07 10:12:31 +0200

    Fix code style in class Compare

--- src/Version/Compare.php
+++ src/Version/Compare.php
@@ -1,14 +1,13 @@
 <?php
 namespace Version;
 
-use Version\Version;
-
 class Compare
 {
-    public function compare( $v1, $v2 )
+    public function compare($v1, $v2)
     {
-        $va1 = ( $v1 instanceof Version ) ? $v1 : Version::parse( $v1 );
-        $va2 = ( $v2 instanceof Version ) ? $v2 : Version::parse( $v2 );
-        return( $va1->compare( $va2 ) );
+        $va1 = ($v1 instanceof Version) ? $v1 : Version::parse($v1);
+        $va2 = ($v2 instanceof Version) ? $v2 : Version::parse($v2);
+
+        return($va1->compare($va2));
     }
 }
